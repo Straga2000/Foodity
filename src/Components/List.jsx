eb69b725bcbc2f317fcdc8aa8ddbcef6120c7528
Bogdan Gusuleac <bgusuleac@gmail.com>
2020-11-13 18:19:34 -0800

    4:19 Working on product list

---
+++ src/Components/List.jsx
@@ -0,0 +1,119 @@
+import React, { Component } from 'react';
+
+import ListElement from './ListElement';
+
+export default class List extends Component {
+    state={}
+    render() {
+        return (
+            // <thead>
+            // <tr>
+            //     <th>#</th>
+            //     <th>Name</th>
+            //     <th>Product</th>
+            //     <th>quantity</th>
+            //     <th>Status</th>
+            // </tr>
+            // </thead>
+
+            <div className="col-lg-12">
+                <div className="card">
+                    <div className="card-title">
+                        <h4>New Orders </h4>
+                    </div>
+                    <div className="card-body">
+                        <div className="table-responsive">
+                            <table className="table">
+                                <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Name</th>
+                                    <th>Product</th>
+                                    <th>quantity</th>
+                                    <th>Status</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td>
+                                        <div className="round-img">
+                                            <a href=""><img className="w-35" src="assets/images/avatar/1.jpg"
+                                                            alt=""></a>
+                                        </div>
+                                    </td>
+                                    <td>Lew Shawon</td>
+                                    <td><span>Dell-985</span></td>
+                                    <td><span>456 pcs</span></td>
+                                    <td><span className="badge badge-success">Done</span></td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div className="round-img">
+                                            <a href=""><img className="w-35" src="assets/images/avatar/1.jpg"
+                                                            alt=""></a>
+                                        </div>
+                                    </td>
+                                    <td>Lew Shawon</td>
+                                    <td><span>Asus-565</span></td>
+                                    <td><span>456 pcs</span></td>
+                                    <td><span className="badge badge-warning">Pending</span></td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div className="round-img">
+                                            <a href=""><img className="w-35" src="assets/images/avatar/1.jpg"
+                                                            alt=""></a>
+                                        </div>
+                                    </td>
+                                    <td>lew Shawon</td>
+                                    <td><span>Dell-985</span></td>
+                                    <td><span>456 pcs</span></td>
+                                    <td><span className="badge badge-success">Done</span></td>
+                                </tr>
+
+                                <tr>
+                                    <td>
+                                        <div className="round-img">
+                                            <a href=""><img className="w-35" src="assets/images/avatar/1.jpg"
+                                                            alt=""></a>
+                                        </div>
+                                    </td>
+                                    <td>Lew Shawon</td>
+                                    <td><span>Asus-565</span></td>
+                                    <td><span>456 pcs</span></td>
+                                    <td><span className="badge badge-warning">Pending</span></td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div className="round-img">
+                                            <a href=""><img className="w-35" src="assets/images/avatar/1.jpg"
+                                                            alt=""></a>
+                                        </div>
+                                    </td>
+                                    <td>lew Shawon</td>
+                                    <td><span>Dell-985</span></td>
+                                    <td><span>456 pcs</span></td>
+                                    <td><span className="badge badge-success">Done</span></td>
+                                </tr>
+
+                                <tr>
+                                    <td>
+                                        <div className="round-img">
+                                            <a href=""><img className="w-35" src="assets/images/avatar/1.jpg"
+                                                            alt=""></a>
+                                        </div>
+                                    </td>
+                                    <td>Lew Shawon</td>
+                                    <td><span>Asus-565</span></td>
+                                    <td><span>456 pcs</span></td>
+                                    <td><span className="badge badge-warning">Pending</span></td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    );
+    }
+    }
--- src/Components/ListElement.jsx
+++ src/Components/ListElement.jsx
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 import '../Styles/ListElement.css';
 
 export default class ListElement extends Component {
-    state={}
+    state = {
+
+    }
+    }
+
     render() {
         return (
-            <h1>Hello</h1>
+                                <tr>
+                                    <td>
+                                        <div className="round-img">
+                                            <a href="">
+                                                <img className="w-35" src="../Images/product.png" alt=""></a>
+                                        </div>
+                                    </td>
+                                    <td>Lew Shawon</td>
+                                    <td><span>Dell-985</span></td>
+                                    <td><span>456 pcs</span></td>
+                                    <td><span className="badge badge-success">Done</span></td>
+                                    </tr>
         );
     }
 }
